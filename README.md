cc3a7477f3de8c0be313fdf0a55d3492e4c34ecf
James Rhodes <jrhodes@redpointsoftware.com.au>
2012-09-02 03:22:11 +1000

    Got QuotaTracker fully working.
    Updated README.
    Reworked database structure.
    Implemented 'quota' command.
    Added systemd and tc management scripts.
    Added 'usage' script.
    Cleaned up code.

--- README.md
+++ README.md
@@ -1,4 +1,59 @@
 QuotaTracker
 ============
 
-Tracks and throttles internet usage using IPTables.
+Tracks and throttles internet usage using IPTables and Traffic Control.  Licensed under MIT.
+
+Installation
+-----------------
+You should install QuotaTracker into `/srv/filter`.  There are provided systemd scripts
+to easily get it set up.  Just copy them across and enable them to have the filtering
+system run at start up.  *Do not start the daemon until you initialize the database.*
+
+You can install into an alternate directory, but you will need to update both the systemd
+scripts and the Python scripts (they automatically chdir to `/srv/filter` so they can be
+called from anywhere).
+
+For convenience, you might also want to create some symbolic links:
+```shell
+ln -s /srv/filter/usage /sbin/qtusage
+ln -s /srv/filter/control /sbin/qtcontrol
+```
+
+Finally, you must run the `reset` command in the control program before starting the
+daemon.  This will create / reset the database so that other parts of QuotaTracker
+work correctly.
+
+Getting Started
+------------------
+Before the system will permit any traffic, you need to define the computers that exist on
+the network.  The daemon will automatically resolve hostnames to IP address when modifying
+iptables, so you should provide hostnames (as they are less likely to change).  If the box
+does not have the same domain set as other computers, use the fully-qualified host name of
+each computer.
+
+**qtcontrol Examples:**
+```
+> computer add james-pc James
+Added computer james-pc (James).
+Assigned computer james-pc a quota of 0 bytes over 0 seconds.
+> computer list
+7 computers configured.
+ - james-pc (James)
+ - storm (Nathan)
+ - leigh-pc (Leigh)
+ - peter-pc (Peter)
+ - nathan-laptop (Nathan)
+ - nathan-pc (Nathan)
+ - nathans-ipod (Nathan)
+> quota set 3600 52428800 # Permit computers to use 50MB in 1 hour.
+Assigned all computers a quota of 52428800 bytes over 3600 seconds.
+```
+
+qtusage shows the status of every computer, and indicates whether they are being throttled
+with the `(\*)` indicator.  It is best used with the `watch` command, like so:
+```
+watch qtusage
+```
+
+If you are not experiencing any throttling, ensure that the `tcconfig` file is set to the
+correct interface (this file also allows you to tweak the throttling speed).
--- commands.py
+++ commands.py
@@ -4,90 +4,119 @@
 # vim: set autoindent:
 
 import sys
+import time
 
 # Abstract base class.
 class Command():
-    def handle(self, conn, args):
+    def handle(self, conn, c, args):
         pass
 
 # Reset command.
 class ResetCommand(Command):
-    def handle(self, conn, args):
+    def handle(self, conn, c, args):
         if (raw_input("Are you sure you want to reset the database? [y/N] ").lower() != "y"):
             print "Not doing anything."
             return
-        print "Resetting database..."
-        c = conn.cursor()
 
         # Delete existing tables.
+        delete = list()
         for r in c.execute("SELECT name FROM sqlite_master"):
-            c.execute("DROP TABLE %s", r)
+            delete.append(r[0])
+        for r in delete:
+            print "Erasing %s.." % r
+            c.execute("DROP TABLE " + r)
+        conn.commit()
 
         # Recreate new tables.
+        print "Recreating tables..."
         c.execute("""
-CREATE TABLE computers
-(
-    ip TEXT,
-    network_id INTEGER,
-    hostname TEXT,
-    person TEXT
-);
-
-CREATE TABLE networks
-(
-    interface TEXT
-);
-
-CREATE TABLE quotas
-(
-    computer_id INTEGER,
-    network_id INTEGER,
-    period INTEGER,
-    quota INTEGER
-);
-
-CREATE TABLE usage
-(
-    computer_id INTEGER,
-    quota_id INTEGER,
-    end_time INTEGER,
-    bytes INTEGER
-);
-""")
+            CREATE TABLE computers
+            (
+                hostname TEXT,
+                person TEXT,
+                period INTEGER,
+                quota INTEGER
+            )""")
+        c.execute("""
+            CREATE TABLE usage
+            (
+                computer_id INTEGER,
+                end_time INTEGER,
+                bytes INTEGER
+            )""")
         conn.commit()
-        c.close()
 
-# Network command.
-class NetworkCommand(Command):
-    def handle(self, conn, args):
-        if (len(args) == 0):
-            print "list - List all networks."
-            print "add <interface> - Add a new network on the specified interface."
-            print "remove <interface> - Remove a network by interface."
+# Computer command.
+class ComputerCommand(Command):
+    def handle(self, conn, c, args):
+        if (len(args) == 0 or args[0].lower() == "help"):
+            print "list - List all computers."
+            print "add <hostname> <person> - Add a new computer mapping on the specified interface."
+            print "delete <hostname> - Remove a computer by interface and hostname."
         elif (args[0].lower() == "list" and len(args) == 1):
-            c = conn.cursor()
-            results = c.execute("SELECT interface FROM networks")
-            print len(results) + " networks configured."
+            results = list()
+            for i in c.execute("SELECT hostname, person FROM computers"):
+                results.append(i)
+            print "%i computers configured." % len(results)
             for i in results:
-                print " - " + i
-            c.close()
-        elif (args[0].lower() == "add" and len(args) == 2):
-            c = conn.cursor()
-            if (len(c.execute("SELECT interface FROM networks WHERE interface = '%s'", args[1])) > 0):
-                print "A network already exists on interface " + args[1] + "."
+                print " - %s (%s)" % (i[0], i[1])
+        elif (args[0].lower() == "add" and len(args) == 3):
+            hostname = args[1].lower()
+            person = args[2]
+            c.execute("SELECT COUNT(hostname) FROM computers WHERE LOWER(hostname) = ?", (hostname,))
+            if (c.fetchone()[0] > 0):
+                print "A computer already exists with hostname %s." % hostname
                 return
-            if (c.execute("INSERT INTO networks (interface) VALUES ('%s')", args[1])):
-                print "Added network on interface " + args[1] + "."
+            if (c.execute("INSERT INTO computers (hostname, person, period, quota) VALUES (LOWER(?), ?, ?, ?)", (hostname, person, 0, 0))):
+                print "Added computer %s (%s)." % (hostname, person)
+                print "Assigned computer %s a quota of 0 bytes over 0 seconds."
             else:
-                print "Failed to add network on interface " + args[1] + "."
+                print "Failed to add computer %s." % hostname
+                return
             conn.commit()
-            c.close()
+        elif (args[0].lower() == "delete" and len(args) == 2):
+            hostname = args[1].lower()
+            c.execute("SELECT COUNT(hostname) FROM computers WHERE LOWER(hostname) = ?", (hostname,))
+            if (c.fetchone()[0] == 0):
+                print "No computer %s exists." % hostname
+                return
+            if (c.execute("DELETE FROM computers WHERE LOWER(hostname) = ?", (hostname,))):
+                print "Deleted computer %s." % hostname
+            else:
+                print "Failed to delete computer %s." % hostname
+            conn.commit()
+        else:
+            print "Invalid number of arguments."
+
+# Quota command.
+class QuotaCommand(Command):
+    def handle(self, conn, c, args):
+        if (len(args) == 0 or args[0].lower() == "help"):
+            print "list - Show quotas assigned to computers."
+            print "set <period> <amount> - Set quotas for all computers."
+            print "reset - Reset the quota timers, forcing a new quota cycle to begin."
+        elif (args[0].lower() == "list" and len(args) == 1):
+            results = list()
+            for i in c.execute("SELECT hostname, period, quota FROM computers"):
+                results.append(i)
+            print "%i computers configured." % len(results)
+            for i in results:
+                print " - %s (%i secs) (%i bytes)" % (i[0], i[1], i[2])
+        elif (args[0].lower() == "set" and len(args) == 3):
+            period = int(args[1])
+            quota = int(args[2])
+            print "Assigned all computers a quota of %i bytes over %i seconds." % (quota, period)
+            c.execute("UPDATE computers SET period = ?, quota = ?", (period, quota))
+            conn.commit()
+        elif (args[0].lower() == "reset" and len(args) == 1):
+            c.execute("UPDATE usage SET end_time = ? WHERE end_time > ?", (time.time(), time.time()))
+            conn.commit()
+            print "Reset all quota timers and archived existing usage."
 
 # Help command.
 class HelpCommand(Command):
-    def handle(self, conn, args):
+    def handle(self, conn, c, args):
         print "reset - Clears and recreates the database."
-        print "network - Control networks."
         print "computer - Control computers."
         print "quota - Control quotas."
         print "usage - Display and examine usage."
